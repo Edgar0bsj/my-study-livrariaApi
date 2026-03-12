bc1e3a0ccb067d34bf6ed11515a0d2b7adca7063
Edgar0bsj <edgarbsj.dev@gmail.com>
2026-03-12 19:57:17 -0300

    Primeiro commit :D

---
+++ src/main/java/com/edgar0bsj/my_study_livrariaApi/livraria/LivrariaApplication.java
@@ -0,0 +1,13 @@
+package com.edgar0bsj.my_study_livrariaApi.livraria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LivrariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LivrariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/edgar0bsj/my_study_livrariaApi/livraria/LivrariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.edgar0bsj.my_study_livrariaApi.livraria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LivrariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
